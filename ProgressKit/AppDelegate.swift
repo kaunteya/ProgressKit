5e4daa1a223c2db5cc97c1af79a951f83aeff001
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2015-06-30 11:27:45 +0530

    Initial Commit

---
+++ ProgressKit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ProgressKit
+//
+//  Created by Kauntey Suryawanshi on 30/06/15.
+//  Copyright (c) 2015 Kauntey Suryawanshi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ProgressKit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProgressKit
+//
+//  Created by Kauntey Suryawanshi on 30/06/15.
+//  Copyright (c) 2015 Kauntey Suryawanshi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
