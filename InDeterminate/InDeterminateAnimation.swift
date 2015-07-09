d8569feae2fd2bdfe66166dafee7a79c6f9248c6
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2015-07-09 17:29:29 +0530

    Indeterminate

---
+++ InDeterminate/InDeterminateAnimation.swift
@@ -0,0 +1,17 @@
+//
+//  InDeterminateAnimation.swift
+//  ProgressKit
+//
+//  Created by Kauntey Suryawanshi on 09/07/15.
+//  Copyright (c) 2015 Kauntey Suryawanshi. All rights reserved.
+//
+
+import Foundation
+
+protocol InDeterminable {
+    
+}
+
+class InDeterminateAnimation: InDeterminable {
+    
+}
