99f861f76073032ffdacddddcacd5bc4f14388b9
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2015-10-26 15:43:06 +0530

    Fixed the paths that were closing automatically

--- ProgressUtils.swift
+++ ProgressUtils.swift
@@ -22,26 +22,20 @@ extension NSBezierPath {
         let points = UnsafeMutablePointer<NSPoint>.alloc(3)
         let numElements = self.elementCount
 
-        if numElements > 0 {
-            var didClosePath = true
-            for index in 0..<numElements {
-                let pathType = self.elementAtIndex(index, associatedPoints: points)
-                switch pathType {
-                case .MoveToBezierPathElement:
-                    CGPathMoveToPoint(path, nil, points[0].x, points[0].y)
-                case .LineToBezierPathElement:
-                    CGPathAddLineToPoint(path, nil, points[0].x, points[0].y)
-                    didClosePath = false
-                case .CurveToBezierPathElement:
-                    CGPathAddCurveToPoint(path, nil, points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y)
-                    didClosePath = false
-                case .ClosePathBezierPathElement:
-                    CGPathCloseSubpath(path)
-                    didClosePath = true
-                }
+        for index in 0..<numElements {
+            let pathType = self.elementAtIndex(index, associatedPoints: points)
+            switch pathType {
+            case .MoveToBezierPathElement:
+                CGPathMoveToPoint(path, nil, points[0].x, points[0].y)
+            case .LineToBezierPathElement:
+                CGPathAddLineToPoint(path, nil, points[0].x, points[0].y)
+            case .CurveToBezierPathElement:
+                CGPathAddCurveToPoint(path, nil, points[0].x, points[0].y, points[1].x, points[1].y, points[2].x, points[2].y)
+            case .ClosePathBezierPathElement:
+                CGPathCloseSubpath(path)
             }
-            if !didClosePath { CGPathCloseSubpath(path) }
         }
+
         points.dealloc(3)
         return path
     }
